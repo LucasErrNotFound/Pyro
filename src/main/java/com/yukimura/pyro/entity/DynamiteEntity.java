c8508129c28059e5f6c8ec7030163b7d5a5f8553
LucasErrNotFound <robertxlucas21@protonmail.com>
2026-04-10 12:33:30 +0800

    feat: add Dynamite entity throwable projectile and entity registry

---
+++ src/main/java/com/yukimura/pyro/entity/DynamiteEntity.java
@@ -0,0 +1,69 @@
+package com.yukimura.pyro.entity;
+
+import net.minecraft.core.particles.ItemParticleOption;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.ThrowableProjectile;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+import org.jspecify.annotations.Nullable;
+
+public class DynamiteEntity extends ThrowableProjectile {
+
+    public DynamiteEntity(Level level, LivingEntity owner) {
+        super(EntityType.SNOWBALL, level);
+    }
+
+    public DynamiteEntity(EntityType<? extends ThrowableProjectile> type, Level level) {
+        super(type, level);
+    }
+
+    public DynamiteEntity(EntityType<? extends ThrowableProjectile> type, double x, double y, double z, Level level) {
+        super(EntityType.SNOWBALL, x, y, z, level);
+    }
+
+    @Override
+    protected void defineSynchedData(SynchedEntityData.Builder entityData) {
+
+    }
+
+    private ParticleOptions getParticleParameters() {
+        ItemStack itemStack = this.getWeaponItem();
+        return itemStack.isEmpty() ? ParticleTypes.CLOUD : new ItemParticleOption(ParticleTypes.ITEM, itemStack.getItem());
+    }
+
+    @Override
+    public void handleEntityEvent(byte id) {
+        if (id == 2 || id == 3) {
+            ParticleOptions particleEffect = this.getParticleParameters();
+
+            for (int i = 0; i < 8; ++i) {
+                this.level().addParticle(particleEffect, this.getX(), this.getY(), this.getZ(), 0.0 , 0.0, 0.0);
+            }
+        }
+    }
+
+    @Override
+    protected void onHitEntity(EntityHitResult hitResult) {
+        super.onHitEntity(hitResult);
+        this.discard();
+        this.level().explode(this, getX(), getY(), getZ(), 1.0f, Level.ExplosionInteraction.TNT);
+    }
+
+    @Override
+    protected void onHit(HitResult hitResult) {
+        super.onHit(hitResult);
+        this.discard();
+        this.level().explode(this, getX(), getY(), getZ(), 1.0f, Level.ExplosionInteraction.TNT);
+    }
+
+    @Override
+    public @Nullable ItemStack getWeaponItem() {
+        return super.getWeaponItem();
+    }
+}
---
+++ src/main/java/com/yukimura/pyro/entity/PyroEntities.java
@@ -0,0 +1,33 @@
+package com.yukimura.pyro.entity;
+
+import com.yukimura.pyro.Pyro;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+
+public class PyroEntities {
+
+    public static final EntityType<DynamiteEntity> DYNAMITE_ENTITY = registerEntity(
+        "dynamite",
+        EntityType.Builder.<DynamiteEntity>of(DynamiteEntity::new, MobCategory.MISC)
+            .sized(0.25f, 0.25f)
+    );
+
+    private static <T extends Entity> EntityType<T> registerEntity(String name, EntityType.Builder<T> builder) {
+        ResourceKey<EntityType<?>> key = ResourceKey.create(Registries.ENTITY_TYPE, Identifier.fromNamespaceAndPath(Pyro.MOD_ID, name));
+        return Registry.register(BuiltInRegistries.ENTITY_TYPE, key, builder.build(key));
+    }
+
+    public static void registerModEntityTypes() {
+        Pyro.LOGGER.info("Registering EntityTypes for " + Pyro.MOD_ID);
+    }
+
+    public static void registerAttributes() {
+        // FabricDefaultAttributeRegistry.register(DYNAMITE_ENTITY, DynamiteEntity.);
+    }
+}
