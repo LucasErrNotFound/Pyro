e8bfe65ddfd193b99cdfe9eef84e2390eac9f5e7
LucasErrNotFound <robertxlucas21@protonmail.com>
2026-04-11 15:28:15 +0800

    feat(client): register ThrownItemRenderer for DynamiteEntity
    Wire up the entity renderer so the dynamite item is visible while
    in flight. Uses EntityRenderers.register directly now that DynamiteEntity
    extends ThrowableItemProjectile and satisfies the ItemSupplier bound.

--- src/client/java/com/yukimura/pyro/client/PyroClient.java
+++ src/client/java/com/yukimura/pyro/client/PyroClient.java
@@ -1,10 +1,14 @@
 package com.yukimura.pyro.client;
 
+import com.yukimura.pyro.entity.PyroEntities;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.client.renderer.entity.ThrownItemRenderer;
 
 public class PyroClient implements ClientModInitializer {
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		EntityRenderers.register(PyroEntities.DYNAMITE_ENTITY, ThrownItemRenderer::new);
 	}
 }
